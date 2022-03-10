58350136ba45332adb4424a92a3fc168d75af8f1
ManjotSingh123-cloud <manjotsingh8894@gmail.com>
2022-03-10 13:18:11 -0500

    Added VGS assignment code

---
+++ config.py
@@ -0,0 +1,8 @@
+HTTPS_PROXY_USERNAME="USmEjwYSuJsrQbvrVzJFz7eX"
+HTTPS_PROXY_PASSWORD="08bd45c3-1ca3-4de1-8cff-bfdcf68e3c14"
+VAULT_ID = "tntdlsy56lo"
+STRIPE_API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+SANDBOX_KEY = "C:\\Users\\toj69\\Downloads\\vgs-demo\\vgs-demo\\sandbox.pem"
+
+
+
